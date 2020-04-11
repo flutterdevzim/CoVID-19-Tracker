463b2be332e1405a8bc9213d11fe0ac8b1c33898
alistair <alistairholmes95@gmail.com>
2020-04-11 07:11:15 +0200

    Set up CarouselSlider for prevention screen. Pics and indicators showing.

--- lib/ui/preventative_measures_screen.dart
+++ lib/ui/preventative_measures_screen.dart
@@ -1,5 +1,53 @@
 import 'package:covid_19_tracker/utils/color_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+final List<String> imgList = [
+  'assets/images/doctor.png',
+  'assets/images/human_contact.png',
+  'assets/images/prevent_corona_small.png',
+];
+
+final List carouselImages = map<Widget>(
+  imgList,
+      (index, i) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(children: <Widget>[
+          Image.asset(i, fit: BoxFit.cover, width: 200),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color.fromARGB(200, 0, 0, 0), Color.fromARGB(0, 0, 0, 0)],
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
+              ),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            child: Text(
+              'No. $index image',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  },
+).toList();
+
+List<T> map<T>(List list, Function handler) {
+  List<T> result = [];
+  for (var i = 0; i < list.length; i++) {
+    result.add(handler(i, list[i]));
+  }
+  return result;
+}
 
 class PreventativeMeasuresScreen extends StatefulWidget {
   @override
@@ -9,6 +57,10 @@ class PreventativeMeasuresScreen extends StatefulWidget {
 
 class _PreventativeMeasuresScreenState
     extends State<PreventativeMeasuresScreen> {
+
+  // Step indicator
+  int _current = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -107,73 +159,83 @@ class _PreventativeMeasuresScreenState
             SizedBox(
               height: 8.0,
             ),
-            Expanded(
-              child: ListView(
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 20.0,
-                      right: 20.0,
-                    ),
-                    child: Container(
-                      height: 300.0,
-                      decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadiusDirectional.circular(15.0),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'TODO',
-                        ),
-                      ),
-                    ),
+            Column(
+              children: <Widget>[
+                CarouselSlider(
+                  items: carouselImages,
+                  viewportFraction: 1.0,
+                  aspectRatio: 2.0,
+                  autoPlay: false,
+                  enlargeCenterPage: false,
+                  onPageChanged: (index) {
+                    setState(() {
+                      _current = index;
+                    });
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: map<Widget>(
+                    imgList,
+                        (index, url) {
+                      return Container(
+                        width: 8.0,
+                        height: 8.0,
+                        margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: _current == index
+                                ? Color.fromRGBO(0, 0, 0, 0.9)
+                                : Color.fromRGBO(0, 0, 0, 0.4)),
+                      );
+                    },
                   ),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Align(
-                    alignment: Alignment.bottomCenter,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  left: 10.0,
+                  right: 10.0,
+                  bottom: 10.0,
+                ),
+                child: FlatButton(
+                  padding: EdgeInsets.all(15.0),
+                  onPressed: () => {},
+                  color: preventivePageBGColor,
+                  child: Center(
                     child: Padding(
                       padding: EdgeInsets.only(
-                        left: 10.0,
-                        right: 10.0,
-                        bottom: 10.0,
+                        left: 30.0,
+                        right: 30.0,
                       ),
-                      child: FlatButton(
-                        padding: EdgeInsets.all(15.0),
-                        onPressed: () => {},
-                        color: preventivePageBGColor,
-                        child: Center(
-                          child: Padding(
-                            padding: EdgeInsets.only(
-                              left: 30.0,
-                              right: 30.0,
-                            ),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  'Steps to protect others',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 5.0,
-                                ),
-                                Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.white,
-                                  size: 12.0,
-                                ),
-                              ],
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Steps to protect others',
+                            style: TextStyle(
+                              color: Colors.white,
                             ),
                           ),
-                        ),
+                          SizedBox(
+                            width: 5.0,
+                          ),
+                          Icon(
+                            Icons.arrow_forward_ios,
+                            color: Colors.white,
+                            size: 12.0,
+                          ),
+                        ],
                       ),
                     ),
                   ),
-                ],
+                ),
               ),
             ),
           ],
