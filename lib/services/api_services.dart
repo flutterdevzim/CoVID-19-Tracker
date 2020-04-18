f7cf55873b6d7fee5dd64ca640d6b5ad4a6dee12
DonaldC <donychinhuru@gmail.com>
2020-04-18 18:10:05 +0200

    update deprecated stats api endpoint

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   final String _baseNewsUrl     = 'https://cvtrackerzw.herokuapp.com/news';
-  final String _baseStatsUrl    = 'https://corona.lmao.ninja/countries';
+  final String _baseStatsUrl    = 'https://corona.lmao.ninja/v2/countries';
 
   Future<List> loadNews() async {
     // get data
